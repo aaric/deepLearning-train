9e667b0944d48f6c653e705ce856a809f4cf47cd
qlhan <qlhan@incarcloud.com>
2021-10-26 13:49:11 +0800

    v0.2

---
+++ lang/lang01.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+# 基于元组的可变参数（*可变参数）
+def sum(*numbers):
+    total = 0.0
+    for num in numbers:
+        total += num
+    return total
+
+
+print("{}".format(sum(1.0, 2.0, 3.0)))
+
+
+# 基于字典的可变参数（**可变参数）
+def show_info(**info):
+    for key, value in info.items():
+        print("{0} - {1}".format(key, value))
+
+
+show_info(name="Kitty", age=18, sex=True)
