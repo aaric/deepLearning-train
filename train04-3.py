e513c9d174809f597a210c57455283e20b567bce
qlhan <qlhan@incarcloud.com>
2021-10-27 14:38:35 +0800

    v0.3

--- train04-3.py
+++ train04-3.py
@@ -7,20 +7,28 @@
 import numpy as np
 from keras.models import load_model
 
-# 0-80: train apply data and predict
+
+# 0-80: load model and predict
+def next_soc_lte80(arr):
+    x_data = np.array(soc_lines).reshape(len(arr) // 30, 30)
+    model = load_model("model/ep22mce_soc_lte80.h5")
+    return model.predict(x_data)
+
+
+# 0-80: train apply model
 soc_lines = [11, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13,
              13, 13, 13]
-x_data = np.array(soc_lines).reshape(1, 30)
+print("next soc: {0}".format(int(next_soc_lte80(soc_lines)[0, 0])))
 
-# 0-80: train model load
-model = load_model("model/ep22mce_soc_lte80.h5")
-print("next soc: {0}".format(int(model.predict(x_data)[0, 0])))
 
-# 0-80: train apply data
+# 0-80: load model and predict
+def next_soc_gte80(arr):
+    x_data = np.array(soc_lines).reshape(len(arr) // 30, 30)
+    model = load_model("model/ep22mce_soc_gte80.h5")
+    return model.predict(x_data)
+
+
+# 0-80: train apply model
 soc_lines = [91, 91, 91, 91, 91, 91, 92, 92, 92, 92, 92, 92, 92, 92, 92, 92, 92, 92, 92, 92, 92, 92, 93, 93, 93, 93, 93,
              93, 93, 93]
-x_data = np.array(soc_lines).reshape(1, 30)
-
-# 0-80: train model load and predict
-model = load_model("model/ep22mce_soc_gte80.h5")
-print("next soc: {0}".format(int(model.predict(x_data)[0, 0])))
+print("next soc: {0}".format(int(next_soc_gte80(soc_lines)[0, 0])))
