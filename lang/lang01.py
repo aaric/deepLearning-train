52ea10109d7edbb71b74d77febd10c0b1e3b7ef8
qlhan <qlhan@incarcloud.com>
2021-10-29 14:52:20 +0800

    v0.4 numpy

--- lang/lang01.py
+++ lang/lang01.py
@@ -1,5 +1,5 @@
 """
-函数
+测试函数
 
 @author Aaric
 @version 0.3.0-SNAPSHOT
@@ -7,14 +7,15 @@
 
 
 # 基于元组的可变参数（*可变参数）
-def sum(*numbers):
+def add(*numbers):
     total = 0.0
     for num in numbers:
         total += num
     return total
 
 
-print("{}".format(sum(1.0, 2.0, 3.0)))
+# print
+print("{}".format(add(1.0, 2.0, 3.0)))
 
 
 # 基于字典的可变参数（**可变参数）
@@ -23,4 +24,42 @@ def show_info(**info):
         print("{0} - {1}".format(key, value))
 
 
+# print
 show_info(name="Kitty", age=18, sex=True)
+
+
+# 函数对象-加法
+def add(m, n):
+    return m + n
+
+
+# 函数对象-减法
+def subtract(m, n):
+    return m - n
+
+
+# 函数对象-函数对象
+def calc(opr):
+    if "+" == opr:
+        return add
+    else:
+        return subtract
+
+
+# print
+print("{0} + {1} = {2}".format(2, 1, calc("+")(2, 1)))
+print("{0} - {1} = {2}".format(2, 1, calc("-")(2, 1)))
+
+
+# 函数对象-lambda
+def calc2(opr):
+    if "+" == opr:
+        return lambda m, n: m + n
+    else:
+        return lambda m, n: m - n
+
+
+# print
+print("{0} + {1} = {2}".format(2, 1, calc2("+")(2, 1)))
+print("{0} - {1} = {2}".format(2, 1, calc2("-")(2, 1)))
+
